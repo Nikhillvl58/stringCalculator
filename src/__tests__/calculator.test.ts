6791eda8f3e09dcb0921c510ce9e1933eaf2e8bf
Nikhillvl58 <nikpatode@gmail.com>
2024-10-16 09:43:25 +0530

    test passed should add any amount of numbers

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -8,4 +8,12 @@ describe('Calculator', () => {
         calculator = new Calculator();
     });
 
+    it('when string is empty should return 0', () => {
+        expect(calculator.add('')).toBe(0);
+    });
+
+    it('should add any amount of numbers', () => {
+        expect(calculator.add('1,2,3')).toBe(6);
+        expect(calculator.add('1,2,3,4,5')).toBe(15);
+    });
 })
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,2 +1,33 @@
 export class Calculator {
+
+    add(input: string): number {
+
+        // if there empty string return first test case
+        if (input === '') return 0;
+
+        //common delimeters used
+        let delimiter = /,|\n/;
+
+        //specific delimiter
+        if (input.startsWith('//')) {
+            const delimiterEnd = input.indexOf('\n');
+            delimiter = new RegExp(input.substring(2, delimiterEnd));
+
+            input = input.substring(delimiterEnd + 1);
+        }
+
+        //extracting the numbers from string to do addition
+        const numbers = input.split(delimiter).map(Number);
+
+        //extracting the negative numbers from string to throw error with negative numbers
+        const negatives = numbers.filter(n => n < 0);
+
+        if (negatives.length > 0) {
+            throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+        }
+
+        //if all goes well then return the sum
+        return numbers.reduce((sum, num) => sum + num, 0);
+    }
+
 }
